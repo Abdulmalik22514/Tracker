1ebf0d474d486acf12dc7b107f308dd2f4faf698
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-04-30 20:50:56 +0100

    chore: Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint', // uses babel-eslint transforms
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+  },
+  env: {
+    node: true, // defines things like process.env when generating through node
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended', // use recommended configs
+    'plugin:react/recommended',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+  },
 };
