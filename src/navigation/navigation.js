68c99a155688955cf0fedbf81909d53414a3ce1d
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-04-30 20:50:10 +0100

    feat: Complete navigation setup

---
+++ src/navigation/navigation.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../screens/Login/login';
+import HomePage from '../screens/Home/home';
+
+const Stack = createNativeStackNavigator();
+
+function Navigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigator;
