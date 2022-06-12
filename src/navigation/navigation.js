d999037894a4903b83bcae703b8430cbfb1efcfe
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-06-12 19:04:00 +0100

    chore: refactor

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../screens/Login/login';
 import HomePage from '../screens/Home/home';
 import History from '../screens/History/history';
+import {COLORS} from '../constants';
 
 const Stack = createNativeStackNavigator();
 
 function Navigator() {
   return (
     <NavigationContainer>
+      <StatusBar translucent={false} backgroundColor={COLORS.grey} />
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={HomePage} />
