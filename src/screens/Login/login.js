dad58f2710c1c80610042abe66ccd6ae116bf448
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-06-14 07:16:26 +0100

    chore: Add login validation

--- src/screens/Login/login.js
+++ src/screens/Login/login.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Alert} from 'react-native';
 import CustomButton from '../../components/CustomButton';
 import CustomInput from '../../components/CustomInput';
 import {COLORS, SIZES} from '../../constants';
 
 const Login = ({navigation}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <CustomInput label="Username" />
-      <CustomInput label="Password" />
+      <CustomInput
+        label="Username"
+        value={username}
+        onChange={username => setUsername(username)}
+      />
+      <CustomInput
+        label="Password"
+        value={password}
+        onChange={password => setPassword(password)}
+        isPassword
+      />
       <CustomButton
         title="Proceed"
-        onPress={() => navigation.navigate('Home')}
+        onPress={() => {
+          if (!username && !password) {
+            Alert.alert('Required fields', 'please input all fields');
+          } else {
+            navigation.navigate('Home');
+            setUsername('');
+            setPassword('');
+          }
+        }}
       />
     </View>
   );
