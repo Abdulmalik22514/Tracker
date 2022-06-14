49821abdb62c13630b73bd7494abb33582120dd3
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2022-06-14 07:16:02 +0100

    chore: Add password visibility

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,14 +1,33 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {TextInput, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import React, {useState} from 'react';
 import {COLORS, SIZES} from '../constants';
 import {FONTS} from '../constants/theme';
 
-const CustomInput = ({label, placeholder}) => {
+const CustomInput = ({label, placeholder, value, onChange, isPassword}) => {
+  const [show, setShow] = useState(false);
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{label}</Text>
-      <TextInput placeholder={placeholder} style={styles.inputContainer} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder={placeholder}
+          style={styles.textInput}
+          value={value}
+          onChangeText={onChange}
+          secureTextEntry={show && isPassword}
+        />
+        {isPassword ? (
+          <TouchableOpacity onPress={() => setShow(!show)} activeOpacity={0.8}>
+            <Text style={styles.showText}>{show ? 'Show' : 'Hide'}</Text>
+          </TouchableOpacity>
+        ) : null}
+      </View>
     </View>
   );
 };
@@ -23,12 +42,22 @@ const styles = StyleSheet.create({
     color: COLORS.black,
     ...FONTS.body5,
   },
+  textInput: {
+    width: '80%',
+    paddingRight: SIZES.font10,
+    ...FONTS.body7,
+  },
   inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     backgroundColor: COLORS.ash,
     height: SIZES.font7 * 3.2,
     borderRadius: 10,
-    width: '100%',
     paddingHorizontal: SIZES.font10,
+  },
+  showText: {
+    color: COLORS.black,
     ...FONTS.body7,
   },
 });
